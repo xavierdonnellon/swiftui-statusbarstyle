0de0979cb5dd3b8362e5ccf7410b91c18abbf5f6
xavierdonnellon <74732530+xavierdonnellon@users.noreply.github.com>
2020-11-19 14:40:29 -0600

    Update StatusBarController.swift

--- StatusBarController.swift
+++ StatusBarController.swift
@@ -1,23 +1,11 @@
-import SwiftUI
+//
+//  StatusBarController.swift
+//  Dating App
+//
+//  Created by Xavier Donnellon on 11/5/20.
+//
 
-extension UIApplication {
-    static var hostingController: HostingController<AnyView>? = nil
-    
-    static var statusBarStyleHierarchy: [UIStatusBarStyle] = []
-    static var statusBarStyle: UIStatusBarStyle = .darkContent
-    
-    //Sets the App to start at rootView
-    func setHostingController(withAppDelegate appDelegate: AppDelegate, rootView: AnyView) {
-        let hostingController = HostingController(rootView: rootView.any())
-        windows.first?.rootViewController = hostingController
-        UIApplication.hostingController = hostingController
-    }
-    
-    static func setStatusBarStyle(_ style: UIStatusBarStyle) {
-        statusBarStyle = style
-        hostingController?.setNeedsStatusBarAppearanceUpdate()
-    }
-}
+import SwiftUI
 
 class HostingController<Content: View>: UIHostingController<Content> {
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -25,11 +13,27 @@ class HostingController<Content: View>: UIHostingController<Content> {
     }
 }
 
+///By wrapping views in a RootView, they will become the app's root view. This will enable setting the statusBarStyle.
+struct RootView<Content: View> : View {
+    var content: Content
+    
+    init(@ViewBuilder content: () -> (Content)) {
+        self.content = content()
+    }
+    
+    var body:some View {
+        EmptyView()
+            .onAppear {
+                UIApplication.shared.setHostingController(rootView: AnyView(content))
+            }
+    }
+}
+
 extension View {
     ///Sets the status bar style color for this view.
     func statusBarStyle(_ style: UIStatusBarStyle) -> some View {
         UIApplication.statusBarStyleHierarchy.append(style)
-
+        print("appending da shit")
         //Once this view appears, set the style to the new style. Once it disappears, set it to the previous style.
         return self.onAppear {
             UIApplication.setStatusBarStyle(style)
@@ -39,5 +43,24 @@ extension View {
             UIApplication.statusBarStyleHierarchy.removeLast()
             UIApplication.setStatusBarStyle(style)
         }
+    }
+}
+
+extension UIApplication {
+    static var hostingController: HostingController<AnyView>? = nil
+    
+    static var statusBarStyleHierarchy: [UIStatusBarStyle] = []
+    static var statusBarStyle: UIStatusBarStyle = .darkContent
+    
+    ///Sets the App to start at rootView
+    func setHostingController(rootView: AnyView) {
+        let hostingController = HostingController(rootView: AnyView(rootView))
+        windows.first?.rootViewController = hostingController
+        UIApplication.hostingController = hostingController
+    }
+    
+    static func setStatusBarStyle(_ style: UIStatusBarStyle) {
+        statusBarStyle = style
+        hostingController?.setNeedsStatusBarAppearanceUpdate()
     }
 }
